20296d2afc06033b39c5c50732b21b420760daf3
sagarmulchandani <sagarmulchandani.sm@gmail.com>
2016-09-17 19:09:23 +0530

    done

--- js/controllers.js
+++ js/controllers.js
@@ -15,6 +15,122 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     $scope.menutitle = NavigationService.makeactive("Dashboard");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
+
+
+
+
+
+    $scope.chartConfig1 = {
+
+      options: {
+          //This is the Main Highcharts chart config. Any Highchart options are valid here.
+          //will be overriden by values specified below.
+          chart: {
+              type: 'column'
+
+          },
+          tooltip: {
+              style: {
+                  padding: 10,
+                  fontWeight: 'bold'
+              }
+          }
+      },
+      //The below properties are watched separately for changes.
+
+      //Series object (optional) - a list of series using normal Highcharts series options.
+      series: [{
+         data: [10, 15]
+      }],
+      //Title configuration (optional)
+      // title: {
+      //     text: 'Example with bold text',
+      //     floating: true,
+      //     align: 'right',
+      //     x: -30,
+      //     y: 30
+      // },
+      //Boolean to control showing loading status on chart (optional)
+      //Could be a string if you want to show specific loading text.
+      loading: false,
+      //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+      //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
+      xAxis: {
+      currentMin: 0,
+      currentMax: 1,
+      title: {text: 'values'}
+      },
+      // yAxis: {
+      // currentMin: 0,
+      // currentMax: 20,
+      // title: {text: 'values'}
+      // },
+      //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
+      useHighStocks: false,
+      //size (optional) if left out the chart will default to size of the div or something sensible.
+      size: {
+       width: 300,
+       height: 300
+      },
+      //function (optional)
+      func: function (chart) {
+       //setup some logic for the chart
+      }
+    };
+
+    $scope.chartConfig = {
+
+    options: {
+        //This is the Main Highcharts chart config. Any Highchart options are valid here.
+        //will be overriden by values specified below.
+        chart: {
+            type: 'line'
+        },
+        tooltip: {
+            style: {
+                padding: 10,
+                fontWeight: 'bold'
+            }
+        }
+    },
+    //The below properties are watched separately for changes.
+
+    //Series object (optional) - a list of series using normal Highcharts series options.
+    series: [{
+       data: [10, 15, 12, 8, 7]
+    }],
+    //Title configuration (optional)
+    // title: {
+    //    text: 'Hello'
+    // },
+    //Boolean to control showing loading status on chart (optional)
+    //Could be a string if you want to show specific loading text.
+    loading: false,
+    //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+    //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
+    xAxis: {
+    currentMin: 0,
+    currentMax: 20,
+    title: {text: 'values'}
+    },
+    yAxis: {
+    currentMin: 0,
+    currentMax: 20,
+    title: {text: 'values'}
+    },
+    //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
+    useHighStocks: false,
+    //size (optional) if left out the chart will default to size of the div or something sensible.
+    size: {
+     width: 670,
+     height: 300
+    },
+    //function (optional)
+    func: function (chart) {
+     //setup some logic for the chart
+    }
+  };
+
 })
 
 .controller('MaterialConstructCtrl', function($scope, TemplateService, NavigationService, $timeout) {
