fb04dcfc8dd82a1c06a22fd20eff53708f2f90b2
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-12-21 16:21:23 +0530

    changes

--- js/controllers.js
+++ js/controllers.js
@@ -764,96 +764,8 @@ angular.module('phonecatControllers', ['templateservicemod', 'ui.select', 'toast
         }
 
 
-        $scope.today = function () {
-            $scope.dt = new Date();
-        };
-        $scope.today();
+    
 
-        $scope.clear = function () {
-            $scope.dt = null;
-        };
-
-        $scope.inlineOptions = {
-            customClass: getDayClass,
-            minDate: new Date(),
-            showWeeks: true
-        };
-
-        $scope.dateOptions = {
-            dateDisabled: disabled,
-            formatYear: 'yy',
-            maxDate: new Date(2020, 5, 22),
-            minDate: new Date(),
-            startingDay: 1
-        };
-
-        // Disable weekend selection
-        function disabled(data) {
-            var date = data.date,
-                mode = data.mode;
-            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-        }
-
-        $scope.toggleMin = function () {
-            $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
-            $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
-        };
-
-        $scope.toggleMin();
-
-        $scope.open1 = function () {
-            $scope.popup1.opened = true;
-        };
-
-        $scope.open2 = function () {
-            $scope.popup2.opened = true;
-        };
-
-        $scope.setDate = function (year, month, day) {
-            $scope.dt = new Date(year, month, day);
-        };
-
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.format = $scope.formats[0];
-        $scope.altInputFormats = ['M!/d!/yyyy'];
-
-        $scope.popup1 = {
-            opened: false
-        };
-
-        $scope.popup2 = {
-            opened: false
-        };
-
-        var tomorrow = new Date();
-        tomorrow.setDate(tomorrow.getDate() + 1);
-        var afterTomorrow = new Date();
-        afterTomorrow.setDate(tomorrow.getDate() + 1);
-        $scope.events = [{
-            date: tomorrow,
-            status: 'full'
-        }, {
-            date: afterTomorrow,
-            status: 'partially'
-        }];
-
-        function getDayClass(data) {
-            var date = data.date,
-                mode = data.mode;
-            if (mode === 'day') {
-                var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
-
-                for (var i = 0; i < $scope.events.length; i++) {
-                    var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
-
-                    if (dayToCheck === currentDay) {
-                        return $scope.events[i].status;
-                    }
-                }
-            }
-
-            return '';
-        }
     })
     .controller('HomebannerCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal) {
         $scope.template = TemplateService.changecontent("homebanner");
@@ -2118,18 +2030,68 @@ angular.module('phonecatControllers', ['templateservicemod', 'ui.select', 'toast
         TemplateService.title = $scope.menutitle;
         $scope.all = {};
         $scope.navigation = NavigationService.getnav();
-        $scope.open1 = function () {
-            $scope.popup1.opened = true;
-        };
-        $scope.popup1 = {
-            opened: false
-        };
 
-        $scope.dateSelection = function(date){
-            console.log("aaa", date);
+
+$scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+
+  $scope.dateOptions = {
+    dateDisabled: disabled,
+     showWeeks: false,
+    formatYear: 'yy',
+    maxDate: new Date(2020, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  };
+
+  // Disable weekend selection
+  function disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  }
+
+ 
+
+  $scope.open1 = function(indexid) {
+    $scope.popup1.opened = true;
+  };
+
+  
+  
+  $scope.popup1 = {
+    opened: false
+  };
+
+
+ 
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
         }
+      }
+    }
 
-        $scope.openDate = function () {
+    return '';
+  }
+
+
+      $scope.openDate = function () {
             $uibModal.open({
                 animation: true,
                 templateUrl: "views/modal/datemodal.html",
@@ -2137,26 +2099,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'ui.select', 'toast
             });
         };
 
-        $scope.convertedPay = [{
-            paymentid: "P003",
-            sellerid: "SB009",
-            amount: "7100",
-            transactionref: "ABC001A, ABC003X",
-            status: "Paid"
-        }, {
-            paymentid: "P004",
-            sellerid: "SB009",
-            amount: "7100",
-            transactionref: "ABC001A, ABC003X",
-            status: "Paid"
-        }, {
-            paymentid: "P005",
-            sellerid: "SB009",
-            amount: "7100",
-            transactionref: "ABC001A, ABC003X",
-            status: "Paid"
-        }];
-
+       
         $scope.showData = false;
         $scope.showText = true;
         $scope.showConverted = function (indexid) {
@@ -2214,6 +2157,8 @@ angular.module('phonecatControllers', ['templateservicemod', 'ui.select', 'toast
         }
 
         $scope.getAllTransactionPayment();
+
+        
 
     })
     .controller('Assign-agencyCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
